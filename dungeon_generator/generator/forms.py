887c59e6cccc2012f2e751026e57c65498f1bc69
Pinkie <473839@mail.muni.cz>
2024-10-06 11:11:47 +0200

    New check that maximum dimension is larger than the minimum one.

--- dungeon_generator/generator/forms.py
+++ dungeon_generator/generator/forms.py
@@ -56,17 +56,20 @@ class DungeonGenerationForm(Form):
     def clean(self):
         data = super().clean()
         room_size = (data.get("room_height"), data.get("room_width"))
-        room_range = (
-            data.get("room_min_height"),
-            data.get("room_max_height"),
-            data.get("room_min_width"),
-            data.get("room_max_width"),
-        )
+        room_min_height = data.get("room_min_height")
+        room_max_height = data.get("room_max_height")
+        room_min_width = data.get("room_min_width")
+        room_max_width = data.get("room_max_width")
+        room_range = (room_min_height, room_max_height, room_min_width, room_max_width)
+        room_min_height_factor = data.get("room_min_height_factor")
+        room_max_height_factor = data.get("room_max_height_factor")
+        room_min_width_factor = data.get("room_min_width_factor")
+        room_max_width_factor = data.get("room_max_width_factor")
         room_factor = (
-            data.get("room_min_height_factor"),
-            data.get("room_max_height_factor"),
-            data.get("room_min_width_factor"),
-            data.get("room_max_width_factor"),
+            room_min_height_factor,
+            room_max_height_factor,
+            room_min_width_factor,
+            room_max_width_factor,
         )
         room_size_selected_method = data.get("room_size_method")
         mapping = {
@@ -82,16 +85,23 @@ class DungeonGenerationForm(Form):
                         f"You selected a room size method '{method}', please "
                         f"fill its arguments."
                     )
-        room_max_height = data.get("room_max_height")
-
         if room_max_height:
+            if room_min_height:
+                if room_max_height < room_min_height:
+                    raise ValidationError(
+                        "Minimum dimension has to be smaller than the maximum one."
+                    )
             if room_max_height > data.get("height"):
                 raise ValidationError(
                     "Maximum room height cannot be larger than the height of the "
                     "dungeon."
                 )
-        room_max_width = data.get("room_max_width")
         if room_max_width:
+            if room_min_width:
+                if room_max_width < room_min_width:
+                    raise ValidationError(
+                        "Minimum dimension has to be smaller than the maximum one."
+                    )
             if room_max_width > data.get("width"):
                 raise ValidationError(
                     "Maximum room width cannot be larger than the width of the "
